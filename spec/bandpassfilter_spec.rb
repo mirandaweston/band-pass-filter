d5112539c307343eefcaea1160b7244d92c0d7d7
mirandaweston <mweston105@yahoo.co.uk>
2023-03-23 19:48:19 +0000

    Test passes: returns soundwave of multiple frequencies

--- spec/bandpassfilter_spec.rb
+++ spec/bandpassfilter_spec.rb
@@ -24,4 +24,10 @@ RSpec.describe BandPassFilter do
     filter = BandPassFilter.new(soundwave, 20, 30)
     expect(filter.generate).to eq [20]
   end
+
+  it 'returns soundwave of frequencies within lower and upper limit' do
+    soundwave = [10, 30, 20, 1200, 60]
+    filter = BandPassFilter.new(soundwave, 30, 50)
+    expect(filter.generate).to eq [30, 30, 30, 50, 50]
+  end
 end
